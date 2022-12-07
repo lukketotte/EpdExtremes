5e2f51d03f2600f15116afaa3a5f08e28aa73a36
lukketotte <lukas.arnroth@gmail.com>
2022-12-07 13:36:22 +0100

    intervals setup

--- julia/Constants/constants.jl
+++ julia/Constants/constants.jl
@@ -13,7 +13,7 @@ function getInterval(prob::Real, p::Real, d::Int)
   elseif d < 10
     return (5, sortByShape(p), sortByProb(prob))
   else
-    return (6, 1, 1)
+    return (0,0,0)
   end
 end
 
@@ -134,51 +134,53 @@ plot!(x, y2)
 
 @time find_zero(x -> qF₁(x, 0.99, 0.6, 1), (1.77,4.5), xatol=1e-4)
 
-pF(1.3, 0.95, 2)
-pF(1.4, 0.9, 2)
-pF(2.1, 0.8, 2)
-pF(3.1, 0.7, 2)
-pF(5.2, 0.6, 2)
-pF(10, 0.5, 2)
-pF(28, 0.4, 2)
+pF(1.4, 0.95, 10)
+pF(1.5, 0.9, 10)
+pF(2.1, 0.8, 10)
+pF(3.4, 0.7, 10)
+pF(5.9, 0.6, 10)
+pF(14, 0.5, 10)
+pF(50, 0.4, 10)
+
+u_95, u_9, u_8, u_7, u_6, u_5, u_4 = 1.4, 1.5, 2.1, 3.4, 5.9, 14, 50;
 
 # recreate d1 as forloop
 shapes = [0.95, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4]
 
-d2 = Array{Dict{String, Any}}([])
+d8 = Array{Dict{String, Any}}([])
 
 for p in shapes
   println(p)
   if p == 0.95
-    x = range(0.1, 1.3, length = 500);
+    x = range(0.1, u_95, length = 500);
     y = pF.(x, 0.95, 1);
     y2 = pF.(x, 0.98, 1);
   elseif p == 0.9
-    x = range(0.1, 1.4, length = 500);
+    x = range(0.1, u_9, length = 500);
     y = pF.(x, 0.9, 1);
     y2 = pF.(x, 0.95, 1);
   elseif p == 0.8
-    x = range(0.1, 2.1, length = 700);
+    x = range(0.1, u_8, length = 800);
     y = pF.(x, 0.8, 1);
     y2 = pF.(x, 0.9, 1);
   elseif p == 0.7
-    x = range(0.1, 3.1, length = 1000);
+    x = range(0.1, u_7, length = 1400);
     y = pF.(x, 0.7, 1);
     y2 = pF.(x, 0.8, 1);
   elseif p == 0.6
-    x = range(0.1, 5.2, length = 1200);
+    x = range(0.1, u_6, length = 1600);
     y = pF.(x, 0.6, 1);
     y2 = pF.(x, 0.7, 1);
   elseif p == 0.5
-    x = range(0.1, 10., length = 1500);
+    x = range(0.1, u_5, length = 2000);
     y = pF.(x, 0.5, 1);
     y2 = pF.(x, 0.6, 1);
   else
-    x = range(0.1, 28., length = 2000);
+    x = range(0.1, u_4, length = 3000);
     y = pF.(x, 0.4, 1);
     y2 = pF.(x, 0.5, 1);
   end
-  append!(d2, [
+  append!(d8, [
     Dict(
       "p" => p,
       "prob" => [0.75, 0.5, 0.25, 0.],
@@ -191,5 +193,14 @@ for p in shapes
   ])
 end
 
-jldsave("./julia/d2.jld2"; d2)
-#load_object("./julia/d1.jld2")
+jldsave("./julia/d10.jld2"; d8)
+
+
+d1 = load_object("./julia/Constants/d1.jld2")
+d2 = load_object("./julia/Constants/d2.jld2")
+d4 = load_object("./julia/Constants/d4.jld2")
+d6 = load_object("./julia/Constants/d6.jld2")
+d10 = load_object("./julia/Constants/d10.jld2")
+d = [d1, d2, d4, d6, d10]
+
+jldsave("./julia/qFinterval.jld2"; d)
---
+++ julia/Constants/qFinterval.jl
@@ -0,0 +1,63 @@
+module QFinterval
+
+export getInterval
+
+using JLD2
+
+const dataPath = "./julia/constants/qFinterval.jld2"
+intervals = load_object(dataPath)
+
+function getInterval(prob::Real, p::Real, d::Int; 
+  intervals::Vector{Vector{Dict{String, Any}}} = intervals)::Tuple{Float64, Float64}
+  if p >= 0.4
+    if d === 1
+      return intervals[1][sortByShape(p)]["interval"][sortByProb(prob)]
+    elseif d === 2
+      return intervals[2][sortByShape(p)]["interval"][sortByProb(prob)]
+    elseif d < 4
+      return intervals[3][sortByShape(p)]["interval"][sortByProb(prob)]
+    elseif d < 6
+      return intervals[4][sortByShape(p)]["interval"][sortByProb(prob)]
+    elseif d < 10
+      return intervals[5][sortByShape(p)]["interval"][sortByProb(prob)]
+    else
+      return (0.01, 100.)
+    end
+  else
+    return (0.01, 100.)
+  end
+end
+
+function sortByShape(p::Real)::Int64
+  if p > 0.95
+    return 1
+  elseif p > 0.9
+    return 2
+  elseif p > 0.8
+    return 3
+  elseif p > 0.7
+    return 4
+  elseif p > 0.6
+    return 5
+  elseif p > 0.5
+    return 6
+  elseif p >= 0.4
+    return 7
+  else
+    return 8
+  end
+end
+
+function sortByProb(prob::Real)::Int64
+  if prob > 0.75
+    return 1
+  elseif prob > 0.5
+    return 2
+  elseif prob >= 0.25
+    return 3
+  else
+    return 4
+  end
+end
+
+end
