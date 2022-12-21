95b9287f836d7360ab69f90fc3e88ebbe02a90f9
alexengberg <54619772+alexengberg@users.noreply.github.com>
2022-12-21 08:20:52 +0100

    copula fcns ver1 no censoring

--- julia/FFT.jl
+++ julia/FFT.jl
@@ -1,4 +1,4 @@
-using SpecialFunctions, LinearAlgebra, QuadGK, Roots, Distributions
+using SpecialFunctions, LinearAlgebra, QuadGK, Roots, Distributions, StatsFuns, MvNormalCDF
 include("./Constants/qFinterval.jl")
 using .QFinterval
 using BenchmarkTools
@@ -50,3 +50,269 @@ rF = function(n::Integer, p::Real, d::Integer)
   ret
 end
 
+
+#####################################
+## Univariate mixture distriubtion ##
+#####################################
+
+# Marginal distribution function (CDF)
+pG1 = function(x, p)
+  xmat = x
+  if isa(xmat, Vector)
+    (n, D) = (size(xmat)[1], 1)
+    xmat = reshape(xmat, n, D)
+  elseif isa(xmat, Matrix)
+    (n, D) = size(xmat)
+  else
+    xmat = reshape([xmat], 1, 1)
+    (n, D) = (1, 1)
+  end  
+  val = Matrix{Float64}(undef, n, D)
+  for i in 1:n
+    for j in 1:D
+      xi = x[i, j]
+      if !ismissing(xi)
+        val[i, j] = quadgk(x -> pG1_fun(x, xi, p, D), 0, 1)[1]
+      end
+    end
+  end
+  if !isa(x, Matrix)
+    val = reshape(val, size(val)[1])
+  end
+  val
+end
+
+pG1_fun = function(prob, x, p, d)
+  StatsFuns.normcdf(0.0, 1.0, sign(x) * exp( log(abs(x)) - log(qF(prob, p, d)) ))
+end
+##
+
+# Marginal quantile function
+qG1 = function(prob, p)
+  prob_mat = prob
+  if isa(prob_mat, Vector)
+    (n, D) = (size(prob_mat)[1], 1)
+    prob_mat = reshape(prob_mat, n, D)
+  elseif isa(prob_mat, Matrix)
+    (n, D) = size(prob_mat)
+  else
+    prob_mat = reshape([prob_mat], 1, 1)
+    (n, D) = (1, 1)
+  end
+  val = Matrix{Float64}(undef, n, D)
+  for i in 1:n
+    for j in 1:D
+      p_i = prob_mat[i, j]
+      if !ismissing(p_i)
+        if p_i <= 0
+          val[i, j] = -Inf
+        else
+          if p_i >= 1
+            val[i, j] = Inf
+          else
+            val[i, j] = find_zero(x -> qG1_fun(x, p_i, p), (-10^2, 10^2))
+          end
+        end
+      end
+    end
+  end
+  if !isa(prob, Matrix)
+    val = reshape(val, size(val)[1])
+  end
+  val
+end
+
+qG1_fun = function(x, prob, p)
+  pG1(x, p) .- prob
+end
+##
+
+# Marginal density function (PDF)
+dG1 = function (x, p)
+  xmat = x
+  if isa(xmat, Vector)
+    (n, D) = (size(xmat)[1], 1)
+    xmat = reshape(xmat, n, D)
+  elseif isa(xmat, Matrix)
+    (n, D) = size(xmat)
+  else
+    xmat = reshape([xmat], 1, 1)
+    (n, D) = (1, 1)
+  end
+  val = Matrix{Float64}(undef, n, D)
+  for i in 1:n
+    for j in 1:D
+      xi = x[i, j]
+      if !ismissing(xi)
+        val[i, j] = quadgk(x -> dG1_fun(x, xi, p, D), 0, 1)[1]
+      end
+    end
+  end
+  if !isa(x, Matrix)
+    val = reshape(val, size(val)[1])
+  end
+  val
+end
+
+dG1_fun = function (prob, x, p, d)
+  log_qF = log( qF(prob, p, d) )
+  exp( StatsFuns.normlogcdf(0.0, 1.0, sign(x) * exp(log(abs(x)) - log_qF) - log_qF) )
+end
+##
+
+# Random generator from marginal distribution G1
+rG1 = function (n, p, d)
+  R = rF(n, p, d)
+  W = rand(Normal(), n)
+  R .* W
+end
+
+
+#################################################################################################################################################
+### Multivariate Distribution function, Partial derivatives, Density Function, and Random generator for the Gaussian scale mixture model X=RW ###
+#################################################################################################################################################
+
+# Multivariate distribution function (CDF)
+pG = function (x, Sigma, p) ### x is an nxD matrix; if x is a vector, it is interpreted as a single D-variate vector (not D independent univariate random variables)
+  if isa(x, Vector)
+    (n, D) = (size(x)[1], 1)
+    x = reshape(x, n, D)
+  elseif isa(x, Matrix)
+    (n, D) = size(x)
+  else
+    x = reshape([x], 1, 1)
+    (n, D) = (1, 1)
+  end
+  val = Vector{Float64}(undef, n)
+  for i in 1:n
+    val[i] = pGi(x[i, :], Sigma, p)
+  end
+  val
+end
+
+pGi = function (xi, Sigma, p)
+  ind_nna = Vector{Int64}(undef, length(xi)) # this should probably be rewritten without loop
+  num_nna = 0
+  for i in eachindex(xi)
+    if !ismissing(xi[i])
+      ind_nna[i] = i
+      num_nna = num_nna + 1
+    else
+      ind_nna[i] = missing
+    end
+  end
+  quadgk(x -> pGi_fun(x, Sigma, p, ind_nna, num_nna), 0, 1)[1]
+end
+
+pGi_fun = function (prob, Sigma, p, ind_nna, num_nna)
+  X = Matrix{Float64}(undef, length(prob), num_nna)
+  for i in eachindex(prob)
+    X[i, :] = xi[ind_nna]
+  end
+  X2 = sign.(X) .* exp.(log.(abs.(X)) .- log(qF(prob, p, D)))
+  gauss_res = Vector{Float64}(undef, size(X2)[1])
+  for i in 1:eachindex(X2[:, 1])
+    gauss_res[i] = mvnormcdf(MvNormal(Sigma[ind_nna, ind_nna]), repeat([-Inf], length(X2[i, :])), X2[i, :])[1]
+  end
+  gauss_res
+end
+##
+
+# Multivariate density function (PDF)
+dG = function (x, Sigma, p)
+  if isa(x, Vector)
+    (n, D) = (size(x)[1], 1)
+    x = reshape(x, n, D)
+  elseif isa(x, Matrix)
+    (n, D) = size(x)
+  else
+    x = reshape([x], 1, 1)
+    (n, D) = (1, 1)
+  end
+  val = Vector{Float64}(undef, n)
+  for i in 1:n
+    val[i] = dGi(x[i, :], Sigma, p, D)
+  end
+  return val
+end
+
+dGi = function (xi, Sigma, p, D)
+  ind_nna = Vector{Int64}(undef, length(xi)) # this should probably be rewritten without loop
+  num_nna = 0
+  for i in eachindex(xi)
+    if !ismissing(xi[i])
+      ind_nna[i] = i
+      num_nna = num_nna + 1
+    else
+      ind_nna[i] = missing
+    end
+  end
+  val = quadgk(x -> dGi_fun(x, p, Sigma, D, ind_nna, num_nna), 0, 1)[1]
+  return val
+end
+
+dGi_fun = function (prob, p, Sigma, D, ind_nna, num_nna)
+  X = Matrix{Float64}(undef, length(p), num_nna)
+  for i in eachindex(p)
+    X[i, :] = xi[ind_nna]
+  end
+  log_qF = log( qF(prob, p, D) )
+  X2 = sign.(X) .* exp.(log.(abs.(X)) .- log_qF)
+  gauss_res = Vector{Float64}(undef, size(X2)[1])
+  for i in 1:eachindex(X2[:, 1])
+    gauss_res[i] = exp( pdf( MvLogNormal(Sigma[ind_nna, ind_nna]), X2[i, :] ) .- num_nna * log_qF)
+  end
+  return gauss_res
+end
+##
+
+# Partial derivatives of the distribution function
+# leaving this for now 2022-12-14 09:22
+# pGI = function (x, I, Sigma, p)
+  
+# end
+
+# dGIi = function (xi, I)
+  
+# end
+
+# Random generator from the joint distribution G
+rG = function (n, d, Sigma, p)
+  R = rF(n, p, d)
+  W = rand(MvNormal(Sigma), n)
+  R .* W
+end
+
+###################################################################################################################################
+### Copula, Partial derivatives, Copula Density Function, and Copula random generator for the Gaussian scale mixture model X=RW ###
+###################################################################################################################################
+
+# Copula distribution (CDF)
+pC = function (u, Sigma, p) # u should be a vector of U(0,1)
+  pG( qG1(u, p), Sigma, p)
+end
+
+# Copula density (PDF)
+dC = function (u, Sigma, p)
+  if isa(u, Vector)
+     u = reshape(u, length(u), 1)
+  elseif isa(u, Matrix)
+    u = u
+  else
+    u = reshape([u], 1, 1)
+  end
+  qG1_val = qG1(u, p)
+  dG(qG1_val, Sigma, p) - sum(dG1(qG1_val, p), dims = 2)
+end
+
+
+# Partial derivatives of the copula distribution C
+# leaving this for the now 2022-12-14
+# dCI = function ()
+  
+# end
+
+# Random generator from the copula
+rC = function (n, d, Sigma, p)
+  pG1(rG(n, d, Sigma, p), p)
+end
