6c9eee4944ceae71411152491b1157509662571f
lukas <lukas.arnroth@gmail.com>
2023-01-26 15:55:50 +0100

    note sure why lower level is 0.1 and not lower

--- julia/Constants/constants.jl
+++ julia/Constants/constants.jl
@@ -38,31 +38,31 @@ end
   for p in shapes
     println("p = $p")
     if p == 0.95
-      x = range(0.1, u_95, length = 1500);
+      x = range(0.01, u_95, length = 1500);
       y = pF.(x, 0.95, dims[i]);
       y2 = pF.(x, 0.99, dims[i]);
     elseif p == 0.9
-      x = range(0.1, u_9, length = 1500);
+      x = range(0.01, u_9, length = 1500);
       y = pF.(x, 0.9, dims[i]);
       y2 = pF.(x, 0.95, dims[i]);
     elseif p == 0.8
-      x = range(0.1, u_8, length = 2000);
+      x = range(0.01, u_8, length = 2000);
       y = pF.(x, 0.8, dims[i]);
       y2 = pF.(x, 0.9, dims[i]);
     elseif p == 0.7
-      x = range(0.1, u_7, length = 2000);
+      x = range(0.01, u_7, length = 2000);
       y = pF.(x, 0.7, dims[i]);
       y2 = pF.(x, 0.8, dims[i]);
     elseif p == 0.6
-      x = range(0.1, u_6, length = 2000);
+      x = range(0.01, u_6, length = 2000);
       y = pF.(x, 0.6, dims[i]);
       y2 = pF.(x, 0.7, dims[i]);
     elseif p == 0.5
-      x = range(0.1, u_5, length = 2500);
+      x = range(0.01, u_5, length = 2500);
       y = pF.(x, 0.5, dims[i]);
       y2 = pF.(x, 0.6, dims[i]);
     else
-      x = range(0.1, u_4, length = 3500);
+      x = range(0.01, u_4, length = 3500);
       y = pF.(x, 0.4, dims[i]);
       y2 = pF.(x, 0.5, dims[i]);
     end
