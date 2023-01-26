07a7df2d1b51b8013f0e35ed05a381e07c4a6fc1
lukas <lukas.arnroth@gmail.com>
2023-01-26 16:13:28 +0100

    should fix intervals

--- julia/Constants/constants.jl
+++ julia/Constants/constants.jl
@@ -26,11 +26,11 @@ end
   quadgk(x -> dF(x,p,d), 0, x)[1]
 end
 
-@everywhere u_95, u_9, u_8, u_7, u_6, u_5, u_4 = 1.45, 1.52, 2.12, 3.44, 5.92, 15, 51;
+@everywhere u_95, u_9, u_8, u_7, u_6, u_5, u_4 = 1.45, 1.52, 2.12, 3.44, 5.92, 20, 75;
 
 # recreate d1 as forloop
 @everywhere shapes = [0.95, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4]
-@everywhere dims = [1,2,4,6,8,10]
+@everywhere dims = [2,4,6,8,10,15]
 
 @sync @distributed for i in eachindex(dims)
   dims[i] |> x -> println("d = $x")
@@ -82,11 +82,10 @@ end
 end
 
 
-d1 = load_object("./julia/Constants/d1.jld2");
-d2 = load_object("./julia/Constants/d2.jld2");
-d4 = load_object("./julia/Constants/d4.jld2");
-d6 = load_object("./julia/Constants/d6.jld2");
+d2 = load_object("./julia/Constants/d1.jld2");
+d4 = load_object("./julia/Constants/d2.jld2");
+d6 = load_object("./julia/Constants/d4.jld2");
+d8 = load_object("./julia/Constants/d6.jld2");
 d10 = load_object("./julia/Constants/d10.jld2");
-d = [d1, d2, d4, d6, d10];
-
-jldsave("./julia/qFinterval.jld2"; d)
+d15 = load_object("./julia/Constants/d15.jld2");
+[d1, d2, d4, d6, d10, d15] |> d -> jldsave("./julia/qFinterval.jld2"; d);
