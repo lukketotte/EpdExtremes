4335c39b23c4d8c1a2d64a56dced2a66b7c702a6
lukketotte <lukas.arnroth@gmail.com>
2023-03-09 14:52:48 +0100

    small stuff

--- julia/debug.jl
+++ julia/debug.jl
@@ -6,29 +6,30 @@ using .MultivariateEpd
 
 # OBS: β is p, following notation in MEPD paper
 f(w::Real, t::Real, β::Real, n::Int) = w^((1-n)/2-1) * (1-w)^((n-1)/2 -1) * exp(-0.5*(t/w)^β);
-g(t::Real, β::Real, n::Int) = t^((n-1)/2) * quadgk(w -> f(w, t, β, n), 0,1; atol = 2e-3)[1];
+g(t::Real, β::Real, n::Int) = t^((n-1)/2) * quadgk(w -> f(w, t, β, n), 0,1; atol = 1e-5)[1];
 K(β::Real, n::Int) = n*gamma(n/2)/(π^(n/2)*gamma(1+n/(2*β))*2^(1+n/(2*β)))
 df(x::Real, β::Real, n::Int) = abs(x) > 1e-10 ? g(x^2, β, n) : g(1e-20, β, n)
-dF(x::Real, β::Real, n::Int, c::Real) = quadgk(y -> c*df(y, β,n),-Inf,x; atol = 2e-3)[1]
+dF(x::Real, β::Real, n::Int, c::Real) = quadgk(y -> c*df(y, β,n),-Inf,x; atol = 1e-5)[1]
 
 qF₁(x::Real, p::Real, β::Real, n::Int, c::Real) = dF(x, β, n, c) - p
 qF(p::Real, β::Real, n::Int, c::Real; intval = 20) = find_zero(x -> qF₁(x, p, β, n, c), (-intval,intval), xatol=2e-3)
 
 
+dF(50, 0.5, 2, 1/c)
 ### censored likelihood on original scale
 
 # test
-d = MvEpd(0.9, [1. 0.2 ; 0.2 1]);
-X = mapslices(sortperm, repd(150,d); dims = 1) ./ (size(X, 1)+1)
+d = MvEpd(0.7, [1. 0.2 ; 0.2 1]);
+n = 200
+X = mapslices(sortperm, repd(n,d); dims = 1) ./ (n+1)
 scatter(X[:, 1], X[:, 2])
 
 vals = zeros(6)
-
 # check vals
 β = 0.5
 c = 2*quadgk(x -> df(x, β, 2), 0, Inf; atol = 2e-3)[1]
 dF(30, β, 2, 1/c) # make sure its close to 1
-U = mapslices(x -> qF.(x, β, 2, 1/c; intval = 30), X; dims = 1);
+U = mapslices(x -> qF.(x, β, 2, 1/c; intval = 40), X; dims = 1);
 vals[1] = sum(logpdf(d, U'))-sum(log.(df.(reshape(U, (prod(size(U)),)), β, 2) ./c))
 
 β = 0.65
