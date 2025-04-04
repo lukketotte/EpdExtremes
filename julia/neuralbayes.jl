c7eb0ab23847a0fddfe23c0f0460b590626e86fb
Lukas <lukas.arnroth@gmail.com>
2025-04-04 14:14:23 +0200

    funkar iaf, estimat av p är dåligt dock

--- julia/neuralbayes.jl
+++ julia/neuralbayes.jl
@@ -1,4 +1,6 @@
 using Flux, NeuralEstimators, Folds, Distributions, LinearAlgebra
+using AlgebraOfGraphics, CairoMakie
+
 
 include("./Distributions/mepd.jl")
 using .MultivariateEpd
@@ -13,10 +15,11 @@ struct Parameters{T} <: ParameterConfigurations
 	L
 end
 
+rand(Gamma(1,1),10)
 ## Bivariate example, diagonal Σ
 function sample(K::Integer)
 	# Sample parameters from the prior
-    p = rand(Gamma(1,2), K)
+    p = rand(Uniform(0.5, 3), K)#rand(Gamma(1,1), K)
     σ1 = rand(InverseGamma(3, 1), K)
     σ2 = rand(InverseGamma(3, 1), K)
 	μ1 = randn(K)
@@ -30,10 +33,9 @@ function simulate(θ, m)
     end
 end
 
-
 n = 2    # dimension of each data replicate (bivariate)
 d = 5    # dimension of the parameter vector θ
-w = 128  # width of each hidden layer 
+w = 64  # width of each hidden layer 
 
 # Final layer has output dimension d and enforces parameter constraints
 final_layer = Parallel(
@@ -43,9 +45,6 @@ final_layer = Parallel(
     Dense(w, 2, softplus)      # σ > 0
 )
 
-typeof(final_layer[1])
-
-
 # Inner and outer networks
 ψ = Chain(Dense(n, w, relu), Dense(w, d, relu))    
 ϕ = Chain(Dense(d, w, relu), final_layer)          
@@ -54,12 +53,18 @@ typeof(final_layer[1])
 network = DeepSet(ψ, ϕ)
 estimator = PointEstimator(network)
 
-m = 100
-estimator = train(estimator, sample, simulate, m = m)
+m = 1000
+K = 2000
+θ_train = sample(K)
+θ_val   = sample(K÷5)
+estimator = train(estimator, θ_train, θ_val, simulate, m = m, epochs = 100)
 
-θ_test = sample(1000)
+θ_test = sample(2000)
 Z_test = simulate(θ_test, m)
-assessment = assess(estimator, θ_test, Z_test, probs = [0.025, 0.975])
+assessment = assess(estimator, θ_test, Z_test)
 
-θ_test[:,1]
-Z_test[1]
+bias(assessment)
+rmse(assessment)
+risk(assessment)
+plot(assessment)
+
